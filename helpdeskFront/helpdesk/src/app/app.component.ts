66d950f4968ac9ba9094d98b98ce173e34ba46dc
Guilherme Alves <guilhermeand21@gmail.com>
2023-11-17 15:22:22 -0300

    Configuração de rota para componente nav

--- helpdeskFront/helpdesk/src/app/app.component.ts
+++ helpdeskFront/helpdesk/src/app/app.component.ts
@@ -2,10 +2,56 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Para trabalhar com formulários no Angular 12
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+// Para realizar requisições HTTP
+import { HttpClientModule } from '@angular/common/http';
+
+// Imports para componentes do Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { NavComponent } from './components/nav/nav.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [MatButtonModule, CommonModule, RouterOutlet,
+  // Forms
+  FormsModule,
+  ReactiveFormsModule,
+  // Requisições http
+  HttpClientModule,
+  // Angular Material
+  MatFormFieldModule,
+  MatPaginatorModule,
+  MatCheckboxModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatInputModule,
+  MatRadioModule,
+  MatTableModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  NavComponent],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
